d67f082d2c5d5acbf6aa3a3945a5451e6c22450d
hariharan <hariharan9572@gmail.com>
2026-08-08 00:49:58 +0530

    second commit

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+class DataTypes{
+    public static void main(String[] args){
+        int a = 10;
+        float b = 20.5f;
+        double c = 30.123456789;
+        char d = 'A';
+        boolean e = true;
+        String f = "Hello, World!";
+        
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Character: " + d);
+        System.out.println("Boolean: " + e);
+        System.out.println("String: " + f);
+    }
+}
