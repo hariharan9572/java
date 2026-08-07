ecd8978f278eef67dfe3717536b16942b824c8c3
hariharan <hariharan9572@gmail.com>
2026-08-08 02:08:25 +0530

    Class and Object Practical

---
+++ ClassAndObject.java
@@ -0,0 +1,17 @@
+class Calculator{
+    public int add(int a, int b){
+        return a + b;
+    }
+}
+
+class ClassAndObject{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+
+        Calculator calc = new Calculator();
+        int sum = calc.add(num1, num2);
+
+        System.out.println(sum);
+    }
+}
